a2fc4ea14f598d866ba700ea110169226bf3b7f8
khakimov mukhammadaziz <khakimovmukhammadaziz@gmail.com>
2026-02-24 13:56:10 +0500

    mainButtonCLicked added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { getData } from "./constants/db";
 import Card from "./components/card/Card";
 import "./App.css";
@@ -45,6 +45,16 @@ const App = () => {
     telegram.MainButton.text = "Sotib olish :)";
     telegram.MainButton.show();
   };
+
+  const onSendData = useCallback(() => {
+    telegram.sendData(JSON.stringify(cartItems));
+  }, [cartItems]);
+
+  useEffect(() => {
+    telegram.onEvent("mainButtonClicked", onSendData);
+
+    return telegram.offEvent("mainButtonClicked", onSendData);
+  }, [onSendData]);
 
   return (
     <>
